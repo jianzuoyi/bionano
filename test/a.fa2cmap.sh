dc1e40580b3b66a26efa546ec2f1c341573e75aa
Zuoyi Jian <jianzuoyi@qq.com>
2018-01-31 11:41:23 +0800

    first commit

---
+++ test/a.fa2cmap.sh
@@ -0,0 +1,6 @@
+fa2map=/its1/GB_BT2/jianzuoyi/biosoft/bionano/tools/pipeline/1.0/HybridScaffold/1.0/scripts/fa2cmap_multi_color.pl
+
+perl $fa2map \
+-i /its1/GB_BT2/jianzuoyi/biosoft/bionano/Examples/Test.fa \
+-e BssSI 1 \
+-o /its1/GB_BT2/jianzuoyi/biosoft/bionano/test/ref_cmap
---
+++ test/b.denovoRef.sh
@@ -0,0 +1,10 @@
+python /its1/GB_BT2/jianzuoyi/biosoft/bionano/tools/pipeline/1.0/Pipeline/1.0/pipelineCL.py \
+-T 4 \
+-f 0.2 \
+-i 5 \
+-y \
+-b /its1/GB_BT2/jianzuoyi/biosoft/bionano/Examples/Molecules.bnx \
+-l /its1/GB_BT2/jianzuoyi/biosoft/bionano/test/denovo_map \
+-t /its1/GB_BT2/jianzuoyi/biosoft/bionano/tools/pipeline/1.0/RefAligner/1.0/ \
+-a /its1/GB_BT2/jianzuoyi/biosoft/bionano/tools/pipeline/1.0/RefAligner/1.0/optArguments_haplotype_saphyr.xml \
+-r /its1/GB_BT2/jianzuoyi/biosoft/bionano/test/ref_cmap/Test_BSSSI_0kb_0labels.cmap
